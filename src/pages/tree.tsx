acc9c1eb43d865c941854306f63ff6a84d84f25d
Ivan Vasilev <20002mc@gmail.com>
2023-08-27 13:08:09 +0300

    simple svg usage

---
+++ src/pages/tree.tsx
@@ -0,0 +1,11 @@
+export default function TreePage() {
+    return <div>
+        <svg width="250" height="250" xmlns="http://www.w3.org/2000/svg">
+            <path d="M150 0 L75 200 L225 200 Z" fill="black" stroke="black"/>
+            <circle cx="10" cy="10" r="2" fill="red"/>
+            <circle cx="90" cy="90" r="2" fill="red"/>
+            <circle cx="90" cy="10" r="2" fill="red"/>
+            <circle cx="10" cy="90" r="2" fill="red"/>
+        </svg>
+    </div>
+}
